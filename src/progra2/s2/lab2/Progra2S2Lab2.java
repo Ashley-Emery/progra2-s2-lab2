a16534208b0a332b9073a747d9ab1e17eeef471a
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-10-23 23:11:23 -0600

    initial commit

---
+++ src/progra2/s2/lab2/Progra2S2Lab2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progra2.s2.lab2;
+
+/**
+ *
+ * @author ashley
+ */
+public class Progra2S2Lab2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
